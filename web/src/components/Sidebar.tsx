bd3eb993d2d0450fc64a727a7a100eb3e47c37cb
SpikeHD <spikegdofficial@gmail.com>
2024-04-03 21:55:38 -0700

    feat: homepage

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,14 +1,17 @@
-import { Link } from 'preact-router'
+import { Link, route } from 'preact-router'
 
 import './Sidebar.css'
 
 export function Sidebar() {
   return (
     <div class="sidebar">
-      <div class="sidebar-icon">
+      <div class="sidebar-icon" onClick={() => {
+        route('/')
+      }}>
         <img src="/icons/omcp_icon.png" alt="logo" />
       </div>
 
+      <Link href="/" class="sidebar-item">Home</Link>
       <Link href="/manage" class="sidebar-item">Servers</Link>
       <Link href="/deploy" class="sidebar-item deploy">Deploy</Link>
     </div>
--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import { route } from 'preact-router';
+import { Icon } from '../../util/icon';
 import './Home.css'
 
 interface Props {
@@ -8,7 +10,22 @@ export function Home(props: Props) {
   return (
     <div class="home">
       <h1>Home</h1>
-      <p>Homepage :D</p>
+      
+      <div class="home-content">
+        <div class="home-item" onClick={() => {
+          route('/deploy')
+        }}>
+          <Icon icon="plus" />
+          <span>DEPLOY A SERVER</span>
+        </div>
+
+        <div class="home-item" onClick={() => {
+          route('/manage')
+        }}>
+          <Icon icon="server-cog" />
+          <span>MANAGE YOUR SERVERS</span>
+        </div>
+      </div>
     </div>
   )
 }
